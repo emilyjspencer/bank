0d17a8403f07e94cfcffa462183d1508d1dcda55
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-03-01 13:33:57 +0000

    Account holder can wtihdraw money

--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -4,8 +4,15 @@ describe BankAccount do
   let(:bankaccount) { BankAccount.new }
 
   describe '#deposit' do
-    it 'allows the account holder to withdraw money' do
+    it 'allows the account holder to deposit money' do
       expect { bankaccount.deposit(20) }.to change{bankaccount.balance}.by(20)
+    end 
+  end 
+
+  describe '#withdraw' do
+    it 'allows the account holder to withdraw money' do
+      bankaccount.deposit(20)
+      expect { bankaccount.withdraw(10) }.to change{bankaccount.balance}.by(-10)
     end 
   end 
 
